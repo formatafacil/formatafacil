9833be4e950a94efa0cadbcc512a41dd6ec44542
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2015-07-16 09:25:20 -0300

    Compilando artigos latex

--- lib/formatafacil/compila.rb
+++ lib/formatafacil/compila.rb
@@ -1,12 +1,14 @@
 module Formatafacil
+  ARTIGO_LATEX = "artigo.tex"
+  ARTIGO_PDF = "artigo.pdf"
+  
   class Compila
-
       def compila_artigo
-        arquivo = "artigo.tex"
-        raise "Erro ao tentar compilar um arquivo que não existe: artigo.tex" unless File.exist?(arquivo)
-        system("/usr/bin/pdflatex -interaction=batchmode artigo.tex")
-        system("/usr/bin/pdflatex -interaction=batchmode artigo.tex")
+        raise "Erro ao tentar compilar um arquivo que não existe: #{ARTIGO_LATEX}" unless File.exist?(ARTIGO_LATEX)
+        system("/usr/bin/pdflatex -interaction=batchmode #{ARTIGO_LATEX}")
+        system("/usr/bin/pdflatex -interaction=batchmode #{ARTIGO_LATEX}")
+        raise "Erro durante a criação do PDF, provavelmente existe erro no arquivo #{ARTIGO_LATEX}" unless File.exist?(ARTIGO_PDF)        
       end
-
+      
   end
 end
--- spec/compila_spec.rb
+++ spec/compila_spec.rb
@@ -5,17 +5,29 @@ describe Formatafacil::Compila do
 
   it 'compila artigo latex com pdflatex' do
     c = Formatafacil::Compila.new
-    expect(File).to receive('exist?').with("artigo.tex") { true }
+    allow(File).to receive('exist?').with("artigo.tex") { true }
+    allow(File).to receive('exist?').with("artigo.pdf") { true }
     expect(c).to receive(:system).with("/usr/bin/pdflatex -interaction=batchmode artigo.tex") { }
     expect(c).to receive(:system).with("/usr/bin/pdflatex -interaction=batchmode artigo.tex") { }
     c.compila_artigo()
   end
 
-  it 'emite erro ao tentar compilar um arquivo latex que não existe' do
-    c = Formatafacil::Compila.new
-    
-    expect(File).to receive('exist?').with("artigo.tex") { false }
-    expect {c.compila_artigo}.to raise_error("Erro ao tentar compilar um arquivo que não existe: artigo.tex")
+  context "emite erro" do
+    it 'se tentar compilar um arquivo latex que não existe' do
+      c = Formatafacil::Compila.new
+      
+      expect(File).to receive('exist?').with("artigo.tex") { false }
+      expect {c.compila_artigo}.to raise_error("Erro ao tentar compilar um arquivo que não existe: artigo.tex")
+    end
+
+    it 'se não gerou um arquivo pdf com sucesso' do
+      c = Formatafacil::Compila.new
+      
+      expect(File).to receive('exist?').with("artigo.tex") { true }
+      expect(File).to receive('exist?').with("artigo.pdf") { false }
+
+      expect {c.compila_artigo}.to raise_error("Erro durante a criação do PDF, provavelmente existe erro no arquivo artigo.tex")
+    end
   end
 
 
